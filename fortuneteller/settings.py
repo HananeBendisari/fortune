08f14124819ee690a6708a57e11bf7f1c744156b
Hanane Bendisari <77987116+HananeBendisari@users.noreply.github.com>
2023-03-08 14:26:19 -0500

    removed secret key, good learning opportunity.
    removing secret key . I don't think this is a big deal at this time as I am not intending on using this project with real data. 
    learning curve, just found out about this secret key issue through a warning email from github. 
    opportunity to learn it and not make the mistake again. will hide it in future projects, learning the process. Installed git-encrypt through homebrew for now.
    As of today, I do not know if just removing the secret from this code here will have any impact on the repo etc. 
    Will see.

--- fortuneteller/settings.py
+++ fortuneteller/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e@a7ie_kb=9gm1n2@ngy0_bn9&!p@^+f7q65qg6g!#sny)-24h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
